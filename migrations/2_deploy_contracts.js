59cade97f74e4042f863f7329751b8dafe0c08aa
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-09-15 14:22:02 -0300

    Updates Addresses.sol

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
-const gcDAI = artifacts.require('gcDAI');
+// const Addresses = artifacts.require('Addresses');
+const GTokenBase = artifacts.require('GTokenBase');
+// const gcDAI = artifacts.require('gcDAI');
 
-module.exports = function(deployer) {
-  deployer.deploy(gcDAI);
+module.exports = (deployer) => {
+  const stakeToken = '0xFcB74f30d8949650AA524d8bF496218a20ce2db4';
+  const reserveToken = '0xF0d0EB522cfa50B716B3b1604C4F0fA6f04376AD';
+  deployer.deploy(GTokenBase, "simple test 1 gcDAI", "st1gcDAI", 18, stakeToken, reserveToken);
+//  deployer.deploy(gcDAI);
 };
