c8de3f62a87ed94f1142c258b0ee23c6a94d2634
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-09-18 14:19:22 -0300

    Makes Curve use only on Mainnet

--- contracts/modules/Conversions.sol
+++ contracts/modules/Conversions.sol
@@ -9,22 +9,28 @@ library Conversions
 {
 	function _calcConversionOutputFromInput(address _from, address _to, uint256 _inputAmount) internal view returns (uint256 _outputAmount)
 	{
-		if (_from == Addresses.DAI && _to == Addresses.USDC) return CurveExchangeAbstraction._calcConversionDAIToUSDCGivenDAI(_inputAmount);
-		if (_from == Addresses.USDC && _to == Addresses.DAI) return CurveExchangeAbstraction._calcConversionUSDCToDAIGivenUSDC(_inputAmount);
+		if (Addresses.NETWORK == Addresses.Network.Mainnet) {
+			if (_from == Addresses.DAI && _to == Addresses.USDC) return CurveExchangeAbstraction._calcConversionDAIToUSDCGivenDAI(_inputAmount);
+			if (_from == Addresses.USDC && _to == Addresses.DAI) return CurveExchangeAbstraction._calcConversionUSDCToDAIGivenUSDC(_inputAmount);
+		}
 		return UniswapV2ExchangeAbstraction._calcConversionOutputFromInput(_from, _to, _inputAmount);
 	}
 
 	function _calcConversionInputFromOutput(address _from, address _to, uint256 _outputAmount) internal view returns (uint256 _inputAmount)
 	{
-		if (_from == Addresses.DAI && _to == Addresses.USDC) return CurveExchangeAbstraction._calcConversionDAIToUSDCGivenUSDC(_outputAmount);
-		if (_from == Addresses.USDC && _to == Addresses.DAI) return CurveExchangeAbstraction._calcConversionUSDCToDAIGivenDAI(_outputAmount);
+		if (Addresses.NETWORK == Addresses.Network.Mainnet) {
+			if (_from == Addresses.DAI && _to == Addresses.USDC) return CurveExchangeAbstraction._calcConversionDAIToUSDCGivenUSDC(_outputAmount);
+			if (_from == Addresses.USDC && _to == Addresses.DAI) return CurveExchangeAbstraction._calcConversionUSDCToDAIGivenDAI(_outputAmount);
+		}
 		return UniswapV2ExchangeAbstraction._calcConversionInputFromOutput(_from, _to, _outputAmount);
 	}
 
 	function _convertFunds(address _from, address _to, uint256 _inputAmount, uint256 _minOutputAmount) internal returns (uint256 _outputAmount)
 	{
-		if (_from == Addresses.DAI && _to == Addresses.USDC) return CurveExchangeAbstraction._convertFundsDAIToUSDC(_inputAmount, _minOutputAmount);
-		if (_from == Addresses.USDC && _to == Addresses.DAI) return CurveExchangeAbstraction._convertFundsUSDCToDAI(_inputAmount, _minOutputAmount);
+		if (Addresses.NETWORK == Addresses.Network.Mainnet) {
+			if (_from == Addresses.DAI && _to == Addresses.USDC) return CurveExchangeAbstraction._convertFundsDAIToUSDC(_inputAmount, _minOutputAmount);
+			if (_from == Addresses.USDC && _to == Addresses.DAI) return CurveExchangeAbstraction._convertFundsUSDCToDAI(_inputAmount, _minOutputAmount);
+		}
 		return UniswapV2ExchangeAbstraction._convertFunds(_from, _to, _inputAmount, _minOutputAmount);
 	}
 }
