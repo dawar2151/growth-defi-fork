f633f5bb69e38081513f10ed674f974fb2c35fcc
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-09-18 21:00:40 -0300

    Fixes withdrawal formulas

--- contracts/GFormulae.sol
+++ contracts/GFormulae.sol
@@ -9,7 +9,7 @@ library GFormulae
 
 	function _calcDepositSharesFromCost(uint256 _cost, uint256 _totalReserve, uint256 _totalSupply, uint256 _depositFee) internal pure returns (uint256 _netShares, uint256 _feeShares)
 	{
-		uint256 _grossShares = _totalReserve == _totalSupply ? _cost : _cost.mul(_totalSupply).div(_totalReserve);
+		uint256 _grossShares = _totalSupply == _totalReserve ? _cost : _cost.mul(_totalSupply).div(_totalReserve);
 		_netShares = _grossShares.mul(1e18).div(uint256(1e18).add(_depositFee));
 		_feeShares = _grossShares.sub(_netShares);
 		return (_netShares, _feeShares);
@@ -25,7 +25,7 @@ library GFormulae
 
 	function _calcWithdrawalSharesFromCost(uint256 _cost, uint256 _totalReserve, uint256 _totalSupply, uint256 _withdrawalFee) internal pure returns (uint256 _grossShares, uint256 _feeShares)
 	{
-		uint256 _netShares = _totalReserve == _totalSupply ? _cost : _cost.mul(_totalSupply).div(_totalReserve);
+		uint256 _netShares = _cost == _totalReserve ? _totalSupply : _cost.mul(_totalSupply).div(_totalReserve);
 		_grossShares = _netShares.mul(1e18).div(uint256(1e18).sub(_withdrawalFee));
 		_feeShares = _grossShares.sub(_netShares);
 		return (_grossShares, _feeShares);
@@ -34,7 +34,7 @@ library GFormulae
 	function _calcWithdrawalCostFromShares(uint256 _grossShares, uint256 _totalReserve, uint256 _totalSupply, uint256 _withdrawalFee) internal pure returns (uint256 _cost, uint256 _feeShares)
 	{
 		uint256 _netShares = _grossShares.mul(uint256(1e18).sub(_withdrawalFee)).div(1e18);
-		_cost = _totalReserve == _totalSupply ? _netShares : _netShares.mul(_totalReserve).div(_totalSupply);
+		_cost = _netShares == _totalSupply ? _totalReserve : _netShares.mul(_totalReserve).div(_totalSupply);
 		_feeShares = _grossShares.sub(_netShares);
 		return (_cost, _feeShares);
 	}
