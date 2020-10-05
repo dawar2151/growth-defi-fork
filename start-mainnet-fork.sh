9daeb84cd62393327a08bc206d2b359b79c8a0d3
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-10-05 13:25:44 -0300

    Fixes ganache-cli script bug and switches to WSS

--- start-mainnet-fork.sh
+++ start-mainnet-fork.sh
@@ -5,8 +5,8 @@ GAS_LIMIT=100000000
 source .env
 
 npx ganache-cli \
-	-h 0.0.0.0
+	-h 0.0.0.0 \
 	-i 1 \
 	-l $GAS_LIMIT \
-	-f https://mainnet.infura.io/v3/$INFURA_PROJECT_ID \
+	-f wss://mainnet.infura.io/ws/v3/$INFURA_PROJECT_ID \
 	--account $PRIVATE_KEY,100000000000000000000
