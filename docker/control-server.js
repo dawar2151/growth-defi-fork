f6bf88a2ba92d1eb9e439f6426ab1b3ba2c376eb
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-10-09 20:15:19 -0300

    Further simplification of the control-server.js script

--- docker/control-server.js
+++ docker/control-server.js
@@ -1,7 +1,7 @@
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
 const child_process = require('child_process');
+
+const port = 8546;
 
 let busy = false;
 
@@ -9,34 +9,8 @@ http.createServer((request, response) => {
 
   console.log(new Date().toISOString(), request.url);
 
-  let filePath = '.' + request.url;
-  if (filePath == './') {
-    filePath = './index.html';
-  }
-
-  const extname = String(path.extname(filePath)).toLowerCase();
-  const mimeTypes = {
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.css': 'text/css',
-    '.json': 'application/json',
-    '.png': 'image/png',
-    '.jpg': 'image/jpg',
-    '.gif': 'image/gif',
-    '.svg': 'image/svg+xml',
-    '.wav': 'audio/wav',
-    '.mp4': 'video/mp4',
-    '.woff': 'application/font-woff',
-    '.ttf': 'application/font-ttf',
-    '.eot': 'application/vnd.ms-fontobject',
-    '.otf': 'application/font-otf',
-    '.wasm': 'application/wasm'
-  };
-
-  const contentType = mimeTypes[extname] || 'application/octet-stream';
-
   if (request.method == 'POST') {
-    if (filePath == './restart') {
+    if (request.url == '/restart') {
       response.writeHead(200, { 'Content-Type': 'text/plain' });
       if (busy) {
         response.end('Server already restarting\n');
@@ -45,49 +19,25 @@ http.createServer((request, response) => {
       busy = true;
       child_process.exec('docker restart ganache-cli', (error, stdout, stderr) => {
         if (error) {
-          console.log(stderr);
           response.end(stderr);
+          console.log(stderr);
           busy = false;
           return;
         }
-        setTimeout(() => {
-          child_process.exec('npm run deploy', (error, stdout, stderr) => {
-            if (error) {
-              console.log(stderr);
-              response.end(stderr);
-              busy = false;
-              return;
-            }
-            console.log(stdout);
-            response.end(stdout);
+        child_process.exec('npm run deploy', (error, stdout, stderr) => {
+          if (error) {
+            response.end(stderr);
+            console.log(stderr);
             busy = false;
-          });
-        }, 15000);
+            return;
+          }
+          response.end(stdout);
+          console.log(stdout);
+          busy = false;
+        });
       });
-      return;
     }
   }
 
-/*
-  fs.readFile(filePath, (error, content) => {
-    if (error) {
-      if(error.code == 'ENOENT') {
-        fs.readFile('./404.html', (error, content) => {
-          response.writeHead(404, { 'Content-Type': 'text/html' });
-          response.end(content, 'utf-8');
-        });
-      }
-      else {
-        response.writeHead(500);
-        response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-      }
-    }
-    else {
-      response.writeHead(200, { 'Content-Type': contentType });
-      response.end(content, 'utf-8');
-    }
-  });
-*/
-
-}).listen(8000);
-console.log('Server running at http://127.0.0.1:8000/');
+}).listen(port);
+console.log('Server running at http://localhost:' + port + '/');
