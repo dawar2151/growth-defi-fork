92126ae2d99b1fa6f95d86e3bb64d39b83884bf5
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-10-16 18:13:51 -0300

    Move the ETH bridge code from gcETH to GEtherBridge

--- contracts/GTokens.sol
+++ contracts/GTokens.sol
@@ -57,38 +57,6 @@ contract gcETH is GCTokenType2
 	{
 	}
 
-	function depositETH() public payable {
-		address _from = msg.sender;
-		uint256 _underlyingCost = msg.value;
-		require(_underlyingCost > 0, "underlying cost must be greater than 0");
-		uint256 _cost = GCFormulae._calcCostFromUnderlyingCost(_underlyingCost, exchangeRate());
-		(uint256 _netShares, uint256 _feeShares) = GFormulae._calcDepositSharesFromCost(_cost, totalReserve(), totalSupply(), depositFee());
-		require(_netShares > 0, "shares must be greater than 0");
-		G.safeWrap(_underlyingCost);
-		G.safeLend(reserveToken, _underlyingCost);
-		require(_prepareDeposit(_cost), "not available at the moment");
-		_mint(_from, _netShares);
-		_mint(address(this), _feeShares.div(2));
-		lpm.gulpPoolAssets();
-	}
-
-	function withdrawETH(uint256 _grossShares) public
-	{
-		address payable _from = msg.sender;
-		require(_grossShares > 0, "shares must be greater than 0");
-		(uint256 _cost, uint256 _feeShares) = GFormulae._calcWithdrawalCostFromShares(_grossShares, totalReserve(), totalSupply(), withdrawalFee());
-		uint256 _underlyingCost = GCFormulae._calcUnderlyingCostFromCost(_cost, exchangeRate());
-		require(_underlyingCost > 0, "underlying cost must be greater than 0");
-		require(_prepareWithdrawal(_cost), "not available at the moment");
-		_underlyingCost = G.min(_underlyingCost, G.getLendAmount(reserveToken));
-		G.safeRedeem(reserveToken, _underlyingCost);
-		G.safeUnwrap(_underlyingCost);
-		_from.transfer(_underlyingCost);
-		_burn(_from, _grossShares);
-		_mint(address(this), _feeShares.div(2));
-		lpm.gulpPoolAssets();
-	}
-
 	receive() external payable {}
 }
 
