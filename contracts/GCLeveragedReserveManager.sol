4f1812d2d99d77cdefc19f2718713ed8e4a83339
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-10-26 12:33:07 -0300

    Adds additional test to avoid getting to a collateralization ratio outside the configured range

--- contracts/GCLeveragedReserveManager.sol
+++ contracts/GCLeveragedReserveManager.sol
@@ -200,10 +200,18 @@ library GCLeveragedReserveManager
 		uint256 _liquidityAmount = G.getFlashLoanLiquidity(_self.underlyingToken);
 		if (_minNewLendAmount > _oldLendAmount) {
 			uint256 _amount = _newLendAmount.sub(_oldLendAmount);
+			{
+				uint256 _minAmount = _minNewLendAmount.sub(_oldLendAmount);
+				require(_liquidityAmount >= _minAmount, "cannot maintain collateralization ratio");
+			}
 			return _self._dispatchFlashLoan(G.min(_amount, _liquidityAmount), 1);
 		}
 		if (_maxNewLendAmount < _oldLendAmount) {
 			uint256 _amount = _oldLendAmount.sub(_newLendAmount);
+			{
+				uint256 _minAmount = _oldLendAmount.sub(_maxNewLendAmount);
+				require(_liquidityAmount >= _minAmount, "cannot maintain collateralization ratio");
+			}
 			return _self._dispatchFlashLoan(G.min(_amount, _liquidityAmount), 2);
 		}
 		return true;
