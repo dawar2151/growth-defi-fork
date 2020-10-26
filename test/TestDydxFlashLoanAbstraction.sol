242dbad3d0986b96d43e2907414cd487a3b1597c
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-10-26 14:14:34 -0300

    Adds a test for Dydx flash loans

--- test/TestDydxFlashLoanAbstraction.sol
+++ test/TestDydxFlashLoanAbstraction.sol
@@ -5,19 +5,19 @@ import { Assert } from "truffle/Assert.sol";
 
 import { Env } from "./Env.sol";
 
-import { AaveFlashLoanAbstraction } from "../contracts/modules/AaveFlashLoanAbstraction.sol";
+import { DydxFlashLoanAbstraction } from "../contracts/modules/DydxFlashLoanAbstraction.sol";
 
-contract TestAaveFlashLoanAbstraction is Env
+contract TestDydxFlashLoanAbstraction is Env
 {
 	function test01() public
 	{
-		uint256 _liquidityAmount1 = AaveFlashLoanAbstraction._getFlashLoanLiquidity(DAI);
+		uint256 _liquidityAmount1 = DydxFlashLoanAbstraction._getFlashLoanLiquidity(DAI);
 		Assert.isAbove(_liquidityAmount1, 0e18, "DAI liquidity must be above 0e18");
 
-		uint256 _liquidityAmount2 = AaveFlashLoanAbstraction._getFlashLoanLiquidity(USDC);
+		uint256 _liquidityAmount2 = DydxFlashLoanAbstraction._getFlashLoanLiquidity(USDC);
 		Assert.isAbove(_liquidityAmount2, 0e18, "USDC liquidity must be above 0e18");
 
-		uint256 _liquidityAmount3 = AaveFlashLoanAbstraction._getFlashLoanLiquidity(GRO);
+		uint256 _liquidityAmount3 = DydxFlashLoanAbstraction._getFlashLoanLiquidity(GRO);
 		Assert.equal(_liquidityAmount3, 0e18, "GRO liquidity must be 0e18");
 	}
 }
