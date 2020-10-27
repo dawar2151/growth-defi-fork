c932fc7e1d02cbe1eaafac499e08efb722170986
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-10-27 13:10:10 -0300

    Removes Type 2 migration

--- migrations/05_deploy_GCDelegatedReserveManager.js
+++
@@ -1,7 +0,0 @@
-const G = artifacts.require('G');
-const GCDelegatedReserveManager = artifacts.require('GCDelegatedReserveManager');
-
-module.exports = async (deployer) => {
-  deployer.link(G, GCDelegatedReserveManager);
-  await deployer.deploy(GCDelegatedReserveManager);
-};
