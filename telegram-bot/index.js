a5a7a6c2aea888611e36b358df7b651b5b5f2a9b
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-12-05 15:57:20 -0300

    Improves the Telegram bot with balances and handling of critical ratios

--- telegram-bot/index.js
+++ telegram-bot/index.js
@@ -185,9 +185,26 @@ async function sendMessage(message) {
 
 // main
 
-const ABI_ERC20 = require('../build/contracts/ERC20.json').abi;
-const ABI_GTOKEN = require('../build/contracts/GToken.json').abi;
-const ABI_GCTOKEN = require('../build/contracts/GCToken.json').abi;
+function getAccount() {
+  const [account] = web3.currentProvider.getAddresses();
+  return account;
+}
+
+async function getCurrentNonce() {
+  const account = getAccount();
+  return await web3.eth.getTransactionCount(account);
+}
+
+async function getEthBalance() {
+  const account = getAccount();
+  const amount = await web3.eth.getBalance(account);
+  return coins(amount, 18);
+}
+
+const ABI_ERC20 = require('../build/contracts/GTokenBase.json').abi;
+const ABI_GTOKEN = require('../build/contracts/GTokenBase.json').abi;
+const ABI_GCTOKEN = require('../build/contracts/GCTokenBase.json').abi;
+const ABI_COMPTROLLER = require('../build/contracts/Comptroller.json').abi;
 
 async function newERC20(address) {
   let self;
@@ -247,32 +264,131 @@ async function newGCToken(address) {
       const _amount = await contract.methods.borrowingReserveUnderlying().call();
       return coins(_amount, underlyingToken.decimals);
     },
+    depositUnderlying: async (cost, gasPrice, nonce) => {
+      const account = getAccount();
+      const _cost = units(cost, self.underlyingToken.decimals);
+      const { transactionHash } = await contract.methods.depositUnderlying(_cost).send({ from: account, gasPrice, nonce });
+      return transactionHash;
+    },
+    withdrawUnderlying: async (grossShares, gasPrice, nonce) => {
+      const account = getAccount();
+      const _grossShares = units(grossShares, self.decimals);
+      const { transactionHash } = await contract.methods.withdrawUnderlying(_grossShares).send({ from: account, gasPrice, nonce });
+      return transactionHash;
+    },
   });
 }
 
+async function newComptroller(address) {
+  let self;
+  const contract = new web3.eth.Contract(ABI_COMPTROLLER, address);
+  return (self = {
+    markets: async (ctoken) => {
+      let { _isListed, _collateralFactorMantissa } = await contract.methods.markets(ctoken).call();
+      _collateralFactorMantissa = _collateralFactorMantissa / 1e18;
+      return { _isListed, _collateralFactorMantissa };
+    },
+  });
+}
+
+const MONITORING_INTERVAL = 60; // 1 minute
+
+const POLLING_TIMEOUT = 20; // 20 seconds
+const POLLING_INTERVAL = 60; // 1 minute
+
+const INITIAL_GAS_PRICE = 50e9; // 50 gwei
+const GAS_PRICE_INCREMENT = 100e9; // 100 gwei
+
+const MININUM_BALANCE_FACTOR = 10; // 10x
+
+const SAFE_TOKEN_MARGIN = {
+  'gcDAI': 0.01, // 1%
+  'gcUSDC': 0.01, // 1%
+  'gcETH': 0.05, // 5%
+  'gcWBTC': 0.05, // 5%
+};
+
+const DEPOSIT_AMOUNT = {
+  'gcDAI': '0.001', // DAI
+  'gcUSDC': '0.001', // USDC
+  'gcETH': '0.00001', // WETH
+  'gcWBTC': '0.00001', // WBTC
+};
+
+async function getMaxCollateralizationRatio(gctoken) {
+  const address = getContractAddress('Comptroller');
+  const comptroller = await newComptroller(address);
+  const { _collateralFactorMantissa } = await comptroller.markets(gctoken.reserveToken.address);
+  return _collateralFactorMantissa;
+}
+
 async function checkVitals(gctoken) {
+  const account = getAccount();
   const lending = Number(await gctoken.lendingReserveUnderlying());
   const borrowing = Number(await gctoken.borrowingReserveUnderlying());
-  const collateralizationRatio = (lending > 0 ? (100 * borrowing) / lending : 0).toFixed(2) + '%';
+  const collateralizationRatio = lending > 0 ? borrowing / lending : 0;
+  const maxCollateralizationRatio = await getMaxCollateralizationRatio(gctoken);
+  const safeMargin = SAFE_TOKEN_MARGIN[gctoken.symbol] || 0.05;
+  const criticalCollateralizationRatio = maxCollateralizationRatio - safeMargin;
+  const underlyingBalance = await gctoken.underlyingToken.balanceOf(account);
+  const underlyingSymbol = await gctoken.underlyingToken.symbol;
   return {
     collateralizationRatio,
+    maxCollateralizationRatio,
+    criticalCollateralizationRatio,
+    underlyingBalance,
+    underlyingSymbol,
   }
 }
 
+async function handleCritical(gctoken) {
+  await sendMessage('<i>Entering critical mode for ' + gctoken.symbol + '</i>');
+  const amount = DEPOSIT_AMOUNT[gctoken.symbol] || '1';
+  const initialNonce = await getCurrentNonce();
+  let nonce = initialNonce;
+  for (let gasPrice = INITIAL_GAS_PRICE; nonce === initialNonce; gasPrice += GAS_PRICE_INCREMENT) {
+    let txhash = '';
+    let error = null;
+    try { txhash = await gctoken.depositUnderlying(amount, gasPrice, nonce); } catch (e) { error = e; }
+    await sendMessage('<i>Deposit amount=' + amount + ' gasPrice=' + gasPrice + ' nonce=' + nonce + (txhash ? ' txhash=' + txhash : '') + (error ? ' error="' + error.message + '"' : '') + '</i>');
+    await sleep(POLLING_INTERVAL * 1000);
+    nonce = await getCurrentNonce();
+  }
+  await sendMessage('<i>Leaving critical mode for ' + gctoken.symbol + '</i>');
+}
+
 const DEFAULT_ADDRESS = {
+  'Comptroller': {
+    'mainnet': '0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B',
+    'kovan': '0x5eAe89DC1C671724A672ff0630122ee834098657',
+  },
   'gcDAI': {
     'mainnet': '0x4085669d375D7EBb225C05F6128e60C19079ee1c',
+    'kovan': '0x8Cde5552602DB7563f424d105217e098d96fce36',
   },
   'gcUSDC': {
     'mainnet': '0x0e93b2D3969A0a6b71CE21Aa5be417cd4cAC38D0',
+    'kovan': '0x151ac053B6EEEB604c957f2E1F69F797834DB39b',
   },
   'gcETH': {
     'mainnet': '0xF510949599b90f78A0B40aae82539D09b9bE9e28',
+    'kovan': '0x9Ca66B0165fF06066cd4f39731bBD2797E4E0691',
   },
   'gcWBTC': {
     'mainnet': '0x1085045eF3f1564e4dA4C7315C0B7448d82d5D32',
+    'kovan': '0xb389dc7A147065c0F0572b8c3340F6F01D427751',
   },
 };
+
+const URL_PREFIX = {
+  'mainnet': 'https://etherscan.io/address/',
+  'ropsten': 'https://ropsten.etherscan.io/address/',
+  'rinkeby': 'https://rinkeby.etherscan.io/address/',
+  'kovan': 'https://kovan.etherscan.io/address/',
+  'goerli': 'https://goerli.etherscan.io/address/',
+  'development': '',
+  'testing': '',
+}
 
 function getContractAddress(name) {
   return (DEFAULT_ADDRESS[name] || {})[network] || require('../build/contracts/' + name + '.json').networks[networkId].address;
@@ -295,13 +411,13 @@ async function main(args) {
   interrupt(async (e) => {
     if (!interrupted) {
       interrupted = true;
-      const message = e instanceof Error ? e.message : String(message);
+      const message = e instanceof Error ? e.message : String(e);
       await sendMessage('<i>Monitoring interrupted (' + message + ')</i>');
       exit();
     }
   });
 
-  await sleep(60 * 1000);
+  await sleep(MONITORING_INTERVAL * 1000);
 
   const names = [
     'gcDAI', 'gcUSDC', 'gcETH', 'gcWBTC',
@@ -311,25 +427,56 @@ async function main(args) {
 
   while (true) {
     let message;
+    let criticalList = [];
     try {
       if (web3 === null) {
         web3 = new Web3(new HDWalletProvider(privateKey, HTTP_PROVIDER_URL[network]));
+        web3.eth.transactionPollingTimeout = POLLING_TIMEOUT;
       }
       if (gctokens === null) {
         gctokens = await getTokens(names);
       }
       const lines = [];
+      {
+        const account = getAccount();
+        const ethBalance = await getEthBalance();
+        lines.push('<a href="' + URL_PREFIX[network] + account + '"><b>Bot Address</b></a>');
+        lines.push('balance: ' + ethBalance + ' ETH');
+        lines.push('');
+      }
       for (const gctoken of gctokens) {
         const vitals = await checkVitals(gctoken);
-        const line = '<a href="https://etherscan.io/address/' + gctoken.address + '"><b>' + gctoken.symbol + '</b></a> <i>' + vitals.collateralizationRatio + '</i>';
-        lines.push(line);
+        const criticalLevel = vitals.collateralizationRatio >= vitals.criticalCollateralizationRatio;
+        const curBalance = Number(vitals.underlyingBalance);
+        const minBalance = MININUM_BALANCE_FACTOR * Number(DEPOSIT_AMOUNT[gctoken.symbol] || '1');
+        const lowBalance = curBalance < minBalance;
+
+        const curRatioText = (100 * vitals.collateralizationRatio).toFixed(2) + '%';
+        const ctcRatioText = (100 * vitals.criticalCollateralizationRatio).toFixed(2) + '%';
+        const maxRatioText = (100 * vitals.maxCollateralizationRatio).toFixed(2) + '%';
+        const ratioText = curRatioText + ' of ' + maxRatioText;
+
+        const curBalanceText = curBalance + ' ' + vitals.underlyingSymbol;
+        const minBalanceText = minBalance + ' ' + vitals.underlyingSymbol;
+
+        lines.push('<a href="' + URL_PREFIX[network] + gctoken.address + '"><b>' + gctoken.symbol + '</b></a>');
+        lines.push('ratio: ' + ratioText);
+        lines.push('balance: ' + curBalanceText);
+        if (criticalLevel) lines.push('!!CRITICAL!! ratio >= ' + ctcRatioText);
+        if (lowBalance) lines.push('!!LOW!! balance < ' + minBalanceText);
+        lines.push('');
+
+	if (criticalLevel) criticalList.push(gctoken);
       }
       message = lines.join('\n');
     } catch (e) {
       message = '<i>Monitoring failure (' + e.message + ')</i>';
     }
     await sendMessage(message);
-    await sleep(60 * 1000);
+    for (const gctoken of criticalList) {
+      await handleCritical(gctoken);
+    }
+    await sleep(MONITORING_INTERVAL * 1000);
   }
 }
 
