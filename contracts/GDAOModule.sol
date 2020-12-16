734979288ffb63dc936da36f6b2afd38736e4009
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-12-16 14:48:39 -0300

    Adds comments to the Gnosis Safe DAO module

--- contracts/GDAOModule.sol
+++ contracts/GDAOModule.sol
@@ -10,6 +10,25 @@ import { G } from "./G.sol";
 
 import { Enum, Safe } from "./interop/Gnosis.sol";
 
+/**
+ * @notice This contract implements a Gnosis Safe extension module to allow
+ *         replacing the multisig signers using the 1-level delegation voting
+ *         provided by stkGRO. Every 24 hours, around 0 UTC, a new voting round
+ *         starts and the candidates appointed in the previous round can become
+ *         the signers of the multisig. This module allows up to 7 signers with
+ *         a minimum of 4 signatures to take any action. There are 3 consecutive
+ *         phases in the process, each occuring at a 24 hour voting round. In
+ *         the first round, stkGRO holders can delegate their votes (stkGRO
+ *         balance) to candidates; vote balance is frozen by the end of that
+ *         round. In the second round, most voted candidates can appoint
+ *         themselves to become signers, replacing a previous candidate from the
+ *         current list. In the third and final round, the list of appointed
+ *         candidates is set as the list of signers to the multisig. The 3
+ *         phases overlap so that, when one list of signers is being set, the
+ *         list for the next day is being build, and yet the votes for
+ *         subsequent day are being counted. See GVoting and GTokenType3 for
+ *         further documentation.
+ */
 contract GDAOModule is ReentrancyGuard
 {
 	using SafeMath for uint256;
@@ -30,6 +49,11 @@ contract GDAOModule is ReentrancyGuard
 	uint256 private votingRound = 0;
 	EnumerableSet.AddressSet private candidates;
 
+	/**
+	 * @dev Constructor for the Gnosis Safe extension module.
+	 * @param _safe The Gnosis Safe multisig contract address.
+	 * @param _votingToken The ERC-20 token used for voting (stkGRO).
+	 */
 	constructor (address _safe, address _votingToken) public
 	{
 		safe = _safe;
@@ -44,32 +68,67 @@ contract GDAOModule is ReentrancyGuard
 		}
 	}
 
+	/**
+	 * @notice Returns the current voting round. This value gets incremented
+	 *         every 24 hours.
+	 * @return _votingRound The current voting round.
+	 */
 	function currentVotingRound() public view returns (uint256 _votingRound)
 	{
 		return block.timestamp.div(VOTING_ROUND_INTERVAL);
 	}
 
+	/**
+	 * @notice Returns the approximate number of seconds remaining until a
+	 *         a new voting round starts.
+	 * @return _timeToNextVotingRound The number of seconds to the next
+	 *                                voting round.
+	 */
 	function timeToNextVotingRound() public view returns (uint256 _timeToNextVotingRound)
 	{
-		return block.timestamp.div(VOTING_ROUND_INTERVAL).add(1).mul(VOTING_ROUND_INTERVAL);
+		uint256 _time = block.timestamp;
+		return _time.div(VOTING_ROUND_INTERVAL).add(1).mul(VOTING_ROUND_INTERVAL).sub(_time);
 	}
 
+	/**
+	 * @notice Returns a boolean indicating whether or not there are pending
+	 *         changes to be applied by calling turnOver().
+	 * @return _hasPendingTurnOver True if there are pending changes.
+	 */
 	function hasPendingTurnOver() internal view returns (bool _hasPendingTurnOver)
 	{
 		uint256 _votingRound = block.timestamp.div(VOTING_ROUND_INTERVAL);
 		return _votingRound > votingRound && !synced;
 	}
 
+	/**
+	 * @notice Returns the current number of appointed candidates in the list.
+	 * @return _count The size of the appointed candidate list.
+	 */
 	function candidateCount() public view returns (uint256 _count)
 	{
 		return candidates.length();
 	}
 
+	/**
+	 * @notice Returns the i-th appointed candidates on the list.
+	 * @return _candidate The address of an stkGRO holder appointed to the
+	 *                    candidate list.
+	 */
 	function candidateAt(uint256 _index) public view returns (address _candidate)
 	{
 		return candidates.at(_index);
 	}
 
+	/**
+	 * @notice Appoints as candidate to be a signer for the multisig,
+	 *         starting on the next voting round. Only the actual candidate
+	 *         can appoint himself and he must have a vote count large
+	 *         enough to kick someone else from the appointed candidate list.
+	 *         No that the first candidate appointment on a round may update
+	 *         the multisig signers with the list from the previous round, if
+	 *         there are changes.
+	 */
 	function appointCandidate() public nonReentrant
 	{
 		address _candidate = msg.sender;
@@ -78,11 +137,26 @@ contract GDAOModule is ReentrancyGuard
 		require(_appointCandidate(_candidate), "candidate not eligible");
 	}
 
+	/**
+	 * @notice Updates the multisig signers with the appointed candidade
+	 *         list from the previous round. Anyone can call this method
+	 *         as soon as a new voting round starts. See hasPendingTurnOver()
+	 *         to figure out whether or not there are pending changes to
+	 *         be applied to the multisig.
+	 */
 	function turnOver() public nonReentrant
 	{
 		require(_closeRound(), "must wait next interval");
 	}
 
+	/**
+	 * @dev Finds the appointed candidates with the least amount of votes
+	 *      for the current list. This is used to find the candidate to be
+	 *      removed when a new candidate with more votes is appointed.
+	 * @return _leastVoted The address of the least voted appointed candidate.
+	 * @return _leastVotes The actual number of votes for the least voted
+	 *                     appointed candidate.
+	 */
 	function _findLeastVoted() internal view returns (address _leastVoted, uint256 _leastVotes)
 	{
 		_leastVoted = address(0);
@@ -99,6 +173,12 @@ contract GDAOModule is ReentrancyGuard
 		return (_leastVoted, _leastVotes);
 	}
 
+	/**
+	 * @dev Checks whether or not we have entered a new voting round before
+	 *      turning over.
+	 * @return _success A boolean indicating if indeed we have entered a
+	 *                  new voting round.
+	 */
 	function _closeRound() internal returns (bool _success)
 	{
 		uint256 _votingRound = block.timestamp.div(VOTING_ROUND_INTERVAL);
@@ -110,6 +190,18 @@ contract GDAOModule is ReentrancyGuard
 		return false;
 	}
 
+	/**
+	 * @dev Implements the logic for appointing a new candidate. It looks
+	 *      for the appointed candidate with the least votes and if the
+	 *      prospect given canditate has strictly more votes, it replaces
+	 *      it on the list. Note that, if the list has less than 7 appointed
+	 *      candidates, the operation always succeeds.
+	 * @param _candidate The given prospect candidate, assumed not to be on
+	 *                   the list.
+	 * @return _success A boolean indicating if indeed the prospect appointed
+	 *                  candidate has enough votes to beat someone on the
+	 *                  list and the operation succeded.
+	 */
 	function _appointCandidate(address _candidate) internal returns(bool _success)
 	{
 		uint256 _candidateCount = candidates.length();
@@ -117,13 +209,24 @@ contract GDAOModule is ReentrancyGuard
 			uint256 _votes = GVoting(votingToken).votes(_candidate);
 			(address _leastVoted, uint256 _leastVotes) = _findLeastVoted();
 			if (_leastVotes >= _votes) return false;
-			candidates.remove(_leastVoted);
+			_success = candidates.remove(_leastVoted);
+			assert(_success);
 		}
-		candidates.add(_candidate);
+		_success = candidates.add(_candidate);
+		assert(_success);
 		synced = false;
 		return true;
 	}
 
+	/**
+	 * @dev Implements the turn over by first adding all the missing
+	 *      candidates from the appointed list to the multisig signers
+	 *      list, and later removing the multisig signers not present
+	 *      in the current appointed list. At last, it sets the minimum
+	 *      number of signers to 4 (or the size of the list if smaller than
+	 *      4). This function is optimized to skip the process if it is
+	 *      in sync, i.e no candidates were appointed since the last update.
+	 */
 	function _turnOver() internal
 	{
 		if (synced) return;
@@ -149,18 +252,42 @@ contract GDAOModule is ReentrancyGuard
 		synced = true;
 	}
 
+	/**
+	 * @dev Adds a signer to the multisig by calling the Gnosis Safe function
+	 *      addOwnerWithThreshold() via the execTransactionFromModule()
+	 *      primitive.
+	 * @param _owner The owner/signer to be added to the multisig.
+	 * @param _threshold The new threshold (minimum number of signers) to be set.
+	 * @return _success A boolean indicating if the operation succeded.
+	 */
 	function _addOwnerWithThreshold(address _owner, uint256 _threshold) internal returns (bool _success)
 	{
 		bytes memory _data = abi.encodeWithSignature("addOwnerWithThreshold(address,uint256)", _owner, _threshold);
 		return Safe(safe).execTransactionFromModule(safe, 0, _data, Enum.Operation.Call);
 	}
 
+	/**
+	 * @dev Removes a signer to the multisig by calling the Gnosis Safe function
+	 *      removeOwner() via the execTransactionFromModule()
+	 *      primitive.
+	 * @param _prevOwner The previous owner/signer in the multisig linked list.
+	 * @param _owner The owner/signer to be added to the multisig.
+	 * @param _threshold The new threshold (minimum number of signers) to be set.
+	 * @return _success A boolean indicating if the operation succeded.
+	 */
 	function _removeOwner(address _prevOwner, address _owner, uint256 _threshold) internal returns (bool _success)
 	{
 		bytes memory _data = abi.encodeWithSignature("removeOwner(address,address,uint256)", _prevOwner, _owner, _threshold);
 		return Safe(safe).execTransactionFromModule(safe, 0, _data, Enum.Operation.Call);
 	}
 
+	/**
+	 * @dev Changes minimum number of signers of the multisig by calling the
+	 *      Gnosis Safe function changeThreshold() via the
+	 *      execTransactionFromModule() primitive.
+	 * @param _threshold The new threshold (minimum number of signers) to be set.
+	 * @return _success A boolean indicating if the operation succeded.
+	 */
 	function _changeThreshold(uint256 _threshold) internal returns (bool _success)
 	{
 		bytes memory _data = abi.encodeWithSignature("changeThreshold(uint256)", _threshold);
